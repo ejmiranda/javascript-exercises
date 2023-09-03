cc7cba783d68301edc7f95a676c9e94f36dc818f
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-02 23:20:34 -0500

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(sourceArray, ...items) {
+  let cleanArray = [];
+  nextSourceItem:
+  for (let sourceItem of sourceArray) {
+    for (let item of items) {
+      if (sourceItem === item) {
+        continue nextSourceItem;
+      }
+    }
+    cleanArray.push(sourceItem);
+  }
+  return cleanArray;
 };
 
 // Do not edit below this line

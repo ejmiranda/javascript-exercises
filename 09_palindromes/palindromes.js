6105715d40e16c3b025a604e50a772b6b3b329aa
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-22 17:48:00 +0000

    Finish 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let array = string.toLowerCase().split('');
+  let cleanArray = array.filter(item => {
+    return (item != ' ' && item != ',' && item != '.' && item != '!');
+  });
+  return (cleanArray.join('') === cleanArray.reverse().join(''));
 };
 
 // Do not edit below this line

4f0f6c90c2f892bd2079e782fccfec878ae9a3cb
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-23 03:40:11 +0000

    Halway through 12 Find The Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+ return people.reduce((oldestPerson, person) => {
+  if (!person.yearOfDeath) {
+    person.age = new Date().getFullYear - person.yearOfBirth;
+  } else {
+    person.age = person.yearOfDeath - person.yearOfBirth;
+  }
+  if (Object.keys(oldestPerson).length === 0) {
+    
+  }
+  
+ })
+}, {};
 
 // Do not edit below this line
 module.exports = findTheOldest;

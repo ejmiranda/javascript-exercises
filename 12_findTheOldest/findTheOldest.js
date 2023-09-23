71431207a344a49ba75c8ba2b384d3f50e652237
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-22 23:30:36 -0500

    Finish 12 Find The Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,16 @@
 const findTheOldest = function(people) {
- return people.reduce((oldestPerson, person) => {
-  if (!person.yearOfDeath) {
-    person.age = new Date().getFullYear - person.yearOfBirth;
-  } else {
-    person.age = person.yearOfDeath - person.yearOfBirth;
-  }
-  if (Object.keys(oldestPerson).length === 0) {
-    
-  }
-  
- })
-}, {};
+  return people.reduce((oldestPerson, person) => {
+    if (!('yearOfDeath' in person)) {
+      person.age = new Date().getFullYear() - person.yearOfBirth;
+    } else {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+    if (!('age' in oldestPerson) || oldestPerson.age < person.age) {
+      oldestPerson = person;
+    }
+    return oldestPerson;
+  }, {});
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
